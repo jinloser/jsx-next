158bfc591393fafb957ec3f434c0730d21898194
jinloser <jinwenqiu@foxmail.com>
2021-05-21 09:01:01 +0800

    111

--- packages/babel-plugin-jsx/src/sugar-fragment.ts
+++ packages/babel-plugin-jsx/src/sugar-fragment.ts
@@ -4,31 +4,31 @@ import type { State } from './interface';
 import { createIdentifier, FRAGMENT } from './utils';
 
 const transformFragment = (
-  path: NodePath<t.JSXElement>,
-  Fragment: t.JSXIdentifier | t.JSXMemberExpression,
+    path: NodePath<t.JSXElement>,
+    Fragment: t.JSXIdentifier | t.JSXMemberExpression,
 ) => {
-  const children = path.get('children') || [];
-  return t.jsxElement(
-    t.jsxOpeningElement(Fragment, []),
-    t.jsxClosingElement(Fragment),
-    children.map(({ node }) => node),
-    false,
-  );
+    const children = path.get('children') || [];
+    return t.jsxElement(
+        t.jsxOpeningElement(Fragment, []),
+        t.jsxClosingElement(Fragment),
+        children.map(({ node }) => node),
+        false,
+    );
 };
 
 export default ({
-  JSXFragment: {
-    enter(path: NodePath<t.JSXElement>, state: State) {
-      const fragmentCallee = createIdentifier(state, FRAGMENT);
-      path.replaceWith(transformFragment(
-        path,
-        t.isIdentifier(fragmentCallee)
-          ? t.jsxIdentifier(fragmentCallee.name)
-          : t.jsxMemberExpression(
-            t.jsxIdentifier((fragmentCallee.object as t.Identifier).name),
-            t.jsxIdentifier((fragmentCallee.property as t.Identifier).name),
-          ),
-      ));
+    JSXFragment: {
+        enter(path: NodePath<t.JSXElement>, state: State) {
+            const fragmentCallee = createIdentifier(state, FRAGMENT);
+            path.replaceWith(transformFragment(
+                path,
+                t.isIdentifier(fragmentCallee)
+                    ? t.jsxIdentifier(fragmentCallee.name)
+                    : t.jsxMemberExpression(
+                        t.jsxIdentifier((fragmentCallee.object as t.Identifier).name),
+                        t.jsxIdentifier((fragmentCallee.property as t.Identifier).name),
+                    ),
+            ));
+        },
     },
-  },
 });
